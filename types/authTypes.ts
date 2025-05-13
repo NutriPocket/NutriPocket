48389244f6ae82f3907449dc379525d84a208d16
Tinchocw <mcwikla@fi.uba.ar>
2025-05-12 21:02:05 -0300

    feat: load anthropometric data

--- types/authTypes.ts
+++ types/authTypes.ts
@@ -2,7 +2,13 @@ export type UserAuth = {
     id: string;
     username: string;
     email: string;
+    weight?: number;
+    height?: number;
+    muscleMass?: number;
+    bodyFatPercentage?: number;
+    boneMass?: number;
     token: string;
+
 };
 
 
