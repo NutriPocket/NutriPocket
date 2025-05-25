a2e6d0ae4ecb327a0e18038b0a2836f80fce668c
Tinchocw <mcwikla@fi.uba.ar>
2025-05-24 21:30:44 -0300

    refactor: Change design

--- app/(app)/mealplan/AddFoodToPlan.tsx
+++ app/(app)/mealplan/AddFoodToPlan.tsx
@@ -1,25 +1,15 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-  Modal,
-} from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 import { useAtom } from "jotai";
 import { authenticatedAtom } from "../../../atoms/authAtom";
 import { MealType } from "../../../types/mealTypes";
 import { useRouter, useLocalSearchParams } from "expo-router";
 import useAxiosInstance from "@/hooks/useAxios";
-import { FAB } from "react-native-paper";
-import { TouchableWithoutFeedback } from "react-native";
-import { Formik } from "formik";
-import { createFoodValidationSchema } from "../../../utils/validationSchemas";
-import { TextInput } from "react-native-paper";
+import { FAB, TouchableRipple } from "react-native-paper";
 import Header from "../../../components/Header";
 import FoodModal from "../../../components/FoodModal";
 import { selectedPlanIdAtom } from "../../../atoms/mealPlanAtom";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function AddFoodToPlan() {
   const { weekDay, mealMoment } = useLocalSearchParams();
@@ -103,33 +93,22 @@ export default function AddFoodToPlan() {
           showsVerticalScrollIndicator={false}
         >
           {foodList.map((food) => (
-            <TouchableOpacity
-              onPress={() => handleAddFoodToPlan(food)}
-              key={food.id}
-              style={{
-                backgroundColor: "#E8F5E9",
-                borderRadius: 14,
-                padding: 18,
-                shadowColor: "#000",
-                shadowOpacity: 0.07,
-                shadowRadius: 6,
-                borderLeftWidth: 6,
-                borderLeftColor: "#287D76",
-                borderWidth: selectedFood?.id === food.id ? 2 : 0,
-                borderColor:
-                  selectedFood?.id === food.id ? "#287D76" : "transparent",
-              }}
-            >
-              <Text
-                style={{ fontSize: 20, fontWeight: "bold", color: "#287D76" }}
+            <View key={food.id} style={{}}>
+              <TouchableRipple
+                onPress={() => handleAddFoodToPlan(food)}
+                style={[styles.momentCard]}
               >
-                {food.name}
-              </Text>
-              <Text style={{ fontSize: 15, color: "#444", marginTop: 4 }}>
-                {food.description}
-              </Text>
-              {/* Puedes agregar más info aquí, como calorías, etc */}
-            </TouchableOpacity>
+                <View>
+                  <Text style={styles.foodName}>{food.name}</Text>
+                  <Text
+                    style={{ fontSize: 15, color: "#444", marginTop: 4 }}
+                    numberOfLines={1}
+                  >
+                    {food.description}
+                  </Text>
+                </View>
+              </TouchableRipple>
+            </View>
           ))}
           {foodList.length === 0 && (
             <Text style={{ color: "#888", fontSize: 16, textAlign: "center" }}>
@@ -139,18 +118,13 @@ export default function AddFoodToPlan() {
         </ScrollView>
         <FAB
           icon="plus"
-          style={{
-            position: "absolute",
-            right: 24,
-            bottom: 32,
-            backgroundColor: "#287D76",
-          }}
+          style={styles.fab}
           color="#fff"
           onPress={() => {
             setShowAddModal(true);
           }}
         />
-        {/* MODAL que sube desde abajo */}
+
         <FoodModal
           showAddModal={showAddModal}
           setShowAddModal={setShowAddModal}
@@ -177,37 +151,29 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
-  modalButton: {
-    borderRadius: 8,
-    width: "48%",
-    alignItems: "center",
-    padding: 10,
-  },
-
-  modalOverlay: {
-    flex: 1,
-    justifyContent: "center",
-    padding: 30,
-  },
-  modalContent: {
-    backgroundColor: "#fff",
-    borderRadius: 16,
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowRadius: 10,
-    padding: 30,
-    gap: 30,
-  },
-  modalTitle: {
-    fontWeight: "bold",
-    color: "#287D76",
-    textAlign: "center",
-    fontSize: 22,
-  },
   error: {
     color: "red",
     textAlign: "left",
     fontSize: 12,
     fontStyle: "italic",
+  },
+  momentCard: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+    padding: 10,
+  },
+  fab: {
+    position: "absolute",
+    right: 24,
+    bottom: 40,
+    backgroundColor: "#287D76",
+  },
+  foodName: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#287D76",
   },
 });
