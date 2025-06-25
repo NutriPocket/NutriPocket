8852dda9786c23130ccf1cfff5265039c11f43ef
MaximoGismondi <maximogismondi@gmail.com>
2025-06-25 16:33:11 -0300

    Feat: logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,12 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import { selectedPlanIdAtom } from "../atoms/mealPlanAtom";
 import { useAtom } from "jotai";
+import { authenticatedAtom } from "@/atoms/authAtom";
 
 type HeaderProps = {
   title?: string;
   showBack?: boolean;
+  showLogout?: boolean;
   backTo?: any | null;
   onBack?: () => void;
 };
@@ -16,49 +18,85 @@ type HeaderProps = {
 export default function Header({
   title = "",
   showBack = true,
+  showLogout = false,
   backTo = null,
   onBack,
 }: HeaderProps) {
   const router = useRouter();
   const [selectedPlanId, setSelectedPlanId] = useAtom(selectedPlanIdAtom);
 
+  const [auth, setIsAuthenticated] = useAtom(authenticatedAtom);
+
+  const handleLogout = () => {
+    if (!auth) return;
+    setIsAuthenticated(null);
+    setSelectedPlanId(null);
+    router.replace("/login");
+  };
+
   return (
     <View
       style={{
-        // flex: 1,
         borderBottomColor: "#eee",
         marginTop: 70,
         paddingHorizontal: 15,
         flexDirection: "row",
-        alignItems: showBack ? "flex-start" : "center",
+        alignItems: "center",
+        justifyContent: showBack ? "space-between" : "center",
       }}
     >
       {showBack && (
-        <TouchableOpacity
-          onPress={async () => {
-            if (onBack) {
-              onBack();
-            } else if (backTo) {
-              setSelectedPlanId(null);
-            } else {
-              console.log("entre a back");
-              router.back();
-            }
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <TouchableOpacity
+            onPress={async () => {
+              if (onBack) {
+                onBack();
+              } else if (backTo) {
+                setSelectedPlanId(null);
+              } else {
+                router.back();
+              }
+            }}
+            style={{ marginRight: 12 }}
+          >
+            <MaterialCommunityIcons
+              name="arrow-left"
+              size={28}
+              color="#287D76"
+            />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+              color: "#287D76",
+            }}
+          >
+            {title}
+          </Text>
+        </View>
+      )}
+
+      {!showBack && (
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+            color: "#287D76",
           }}
-          style={{ marginRight: 12 }}
         >
-          <MaterialCommunityIcons name="arrow-left" size={28} color="#287D76" />
+          {title}
+        </Text>
+      )}
+
+      {showLogout && (
+        <TouchableOpacity
+          onPress={handleLogout}
+          style={{ position: "absolute", right: 15 }}
+        >
+          <MaterialCommunityIcons name="logout" size={28} color="#287D76" />
         </TouchableOpacity>
       )}
-      <Text
-        style={{
-          fontSize: 20,
-          fontWeight: "bold",
-          color: "#287D76",
-        }}
-      >
-        {title}
-      </Text>
     </View>
   );
 }
